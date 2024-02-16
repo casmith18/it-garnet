17f11afcc2f0ee43189f29fdc2af07fbb301d36c
Carson Smith <90855545+casmith18@users.noreply.github.com>
2024-02-16 00:49:15 +0000

    finished calculator

--- calculator/assets/js/script.js
+++ calculator/assets/js/script.js
@@ -1,17 +1,14 @@
 function calculate() {
     
-    /* Make sure that the form is valid */
+
     if ($( "#myform" ).valid()) {
         
-        /* get the operands from the form */
         var operand1 = document.getElementById("Operand1").value;
         var operand2 = document.getElementById("Operand2").value;
         
-        /* convert the operands from string to floating point */
         var operand1fp = parseFloat (operand1);
         var operand2fp = parseFloat (operand2);
         
-        /* figure out which operator was checked and place the value in operator */
         var operator;
         if (document.getElementById("AddOperator").checked) {
             operator = document.getElementById("AddOperator").value;
@@ -28,54 +25,40 @@ function calculate() {
 
         var result;
         
-        /* if the operator was "Min" then set result to the minimum */
         if (operator == "Add") {
            result = operand1fp + operand2fp;
-           
-           
-            /* if(operand1fp + operand2fp) {
-                result = operand1fp;
-            }
-            if(operand2fp + operand1fp) {
-                result = operand2fp;
-            }*/
         }
  
-        /* if the operator was "Max" then set result to the maximum */
         if (operator == "Sub") {
-            if(operand1fp >= operand2fp && operand1fp >= operand3fp) {
-                result = operand1fp;
-            }
-            if(operand2fp >= operand1fp && operand2fp >= operand3fp) {
-                result = operand2fp;
-            }
-            if(operand3fp >= operand1fp && operand3fp >= operand2fp) {
-                result = operand3fp;
-            }
+          result = operand1fp - operand2fp;
         }
 
-        /* if operator was "Avg" the calcualute the average of 3 operands */
         if (operator == "Mul") {
-            result = (operand1fp + operand2fp + operand3fp) / 3.0;
+            result = operand1fp * operand2fp;
         }
         
-        /* convert the result to a string and display it */
+        if (operator == "Div") {
+            result = operand1fp / operand2fp;
+        }
+
+      
         document.getElementById("Result").innerHTML = result.toString();
     }
 }
 
 function clearform() {
-    
     document.getElementById("Operand1").value = "";
     document.getElementById("Operand2").value = "";
     document.getElementById("Operand1Error").innerHTML = "";
     document.getElementById("Operand2Error").innerHTML = "";
     document.getElementById("AddOperator").checked = false;
-    document.getElementById("Subperator").checked = false;
+    document.getElementById("SubOperator").checked = false;
     document.getElementById("MulOperator").checked = false;
     document.getElementById("DivOperator").checked = false;
     document.getElementById("OperatorError").innerHTML = "";
     document.getElementById("Result").innerHTML = "";
+
+    console.log("Form cleared"); // Debugging statement
 }
 
 $( "#myform" ).validate({
