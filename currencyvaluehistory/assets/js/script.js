b23ed527833f922d9dad6ade43eae00996c64712
Carson Smith <90855545+casmith18@users.noreply.github.com>
2024-03-14 03:39:47 +0000

    worked on micro site and currency value

---
+++ currencyvaluehistory/assets/js/script.js
@@ -0,0 +1,98 @@
+
+
+
+async function GetStock() {
+    "use strict";
+
+    // Get a reference to the form - Use the ID of the form
+    var form = $("#myform");
+    
+    // Validate all of the for elements
+    form.validate();
+    
+    // If all of the form elements are valid, the get the form values
+    if (form.valid()) {
+        
+        var BaseCurrency = document.getElementById("BaseCurrency").value;
+        var ConvertCurrency = document.getElementById("ConvertCurrency").value;
+        var apiKey = "6mtpNBU2DD4kX7WPDRtge9pKWupI0Ld7";
+        var FromDate = document.getElementById("FromDate").value;
+        var ToDate = document.getElementById("ToDate").value;
+
+       
+        /* URL for AJAX Call */
+        var myURL2 = "https://api.polygon.io/v2/aggs/ticker/C:" + BaseCurrency + ConvertCurrency + "/range/1/day/" + FromDate + "/" + ToDate + "?adjusted=true&sort=asc&limit=120&apiKey=" + apiKey;
+        /* Make the AJAX call */
+        var msg2Object = await fetch(myURL2);
+        /* Check the status */
+        if (msg2Object.status >= 200 && msg2Object.status <= 299) {            
+            var msg2JSONText = await msg2Object.text();
+            // Parse the JSON string into an object
+            var msg2 = JSON.parse(msg2JSONText);
+            /* Your code to process the result goes here - 
+               display the returned message */
+                /* Your code to process the result goes here  
+                    display the returned message */
+                var stockdate = [];
+                var stockvalue = [];
+                var numdays = msg2.results.length;
+                if (numdays > 0) {
+                    for (var i = 0; i < numdays; i++) {
+                        /* stock close value */
+                        stockvalue[i] = msg2.results[i].c;
+                        /* stock volume */
+                        var tempdate = new Date(msg2.results[i].t);
+                        /* extract the date string from the value */
+                        stockdate[i] = tempdate.toLocaleDateString();
+                    }
+                }
+
+             
+                
+              
+
+                var ctx0 = document.getElementById("chartjs-0");
+                var myChart = new Chart(ctx0, {
+                    "type":"line",
+                    "data": {
+                        "labels": stockdate,
+                        "datasets":[{"label":"Base Currency to Convert Currency",
+                        "data": stockvalue,
+                        "fill":false,
+                        "borderColor":"rgb(75, 192, 192)",
+                        "lineTension":0.1}]},
+                        "options":{ 
+                            responsive: false,
+                            maintainAspectRatio: true,
+                        }
+                    }
+                );
+                
+            
+        }
+        else {
+            /* AJAX completed with error - probably invalid stock ticker symbol */
+            alert("Stock Not Found - Status: " + msg2Object.status)
+            return
+        }
+    }
+}
+
+function ClearForm() {
+    document.getElementById("BaseCurrency").value = "";
+    document.getElementById("ConvertCurrency").value = "";
+    document.getElementById("FromDate").value = "";
+    document.getElementById("ToDate").value = "";
+    document.getElementById("BaseCurrencyError").innerHTML = "";
+    document.getElementById("ConvertCurrencyError").innerHTML = "";
+    document.getElementById("FromDateError").innerHTML = "";
+    document.getElementById("ToDateError").innerHTML = "";
+    
+    /* Ugly Code to Erase Canvas */
+    var canvas0 = document.getElementById("chartjs-0");
+    var context0 = canvas0.getContext('2d');    
+    context0.clearRect(0, 0, canvas0.width, canvas0.height);
+    var canvas1 = document.getElementById("chartjs-1");
+    var context1 = canvas1.getContext('2d');    
+    context1.clearRect(0, 0, canvas1.width, canvas1.height);
+}
